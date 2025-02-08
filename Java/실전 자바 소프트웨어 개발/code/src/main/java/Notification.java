fb59d26440ecf5737bf1db538f71d2027b7dc640
yeonee911 <128215774+yeonee911@users.noreply.github.com>
2025-02-08 21:19:09 +0900

    feat: Notification 패턴

--- Java/실전 자바 소프트웨어 개발/code/src/main/java/Notification.java
+++ Java/실전 자바 소프트웨어 개발/code/src/main/java/Notification.java
@@ -1,10 +1,16 @@
 package main.java;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Notification {
 	private final List<String> errors = new ArrayList<>();
+	private String description;
+	private String date;
+	private String amount;
+
 	public void addError(final String message) {
 		errors.add(message);
 	}
@@ -19,5 +25,34 @@ public class Notification {
 
 	public List<String> getErrors() {
 		return this.errors;
+	}
+
+	public Notification validate() {
+
+		final Notification notification = new Notification();
+		if (this.description.length() > 100) {
+			notification.addError("The description is too long");
+		}
+
+		final LocalDate parsedDate;
+		try {
+			parsedDate = LocalDate.parse(this.date);
+			if (parsedDate.isAfter(LocalDate.now())) {
+				notification.addError("date cannot be in the future");
+			}
+		}
+		catch (DateTimeParseException e) {
+			notification.addError("Invalid format for date");
+		}
+
+		final double amount;
+		try {
+			amount = Double.parseDouble(this.amount);
+		}
+		catch(NumberFormatException e) {
+			notification.addError("Invalid format for amount");
+		}
+
+		return notification;
 	}
 }
