d2744647ae611d3dafac13de0f27e5cc32754cbb
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-05-14 02:37:10 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n; cin >> n;
+	vector<int> dp(n + 1, 0);
+	dp[1] = 1;
+	dp[2] = 3;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+	}
+
+	cout << dp[n];
+}
