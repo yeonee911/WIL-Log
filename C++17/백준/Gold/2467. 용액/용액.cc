4b555bde7ab4bf9c3b05244d46c67b632fe9f4f0
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-05-31 01:18:03 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

--- C++17/백준/Gold/2467. 용액/용액.cc
+++ C++17/백준/Gold/2467. 용액/용액.cc
@@ -17,48 +17,19 @@ int main() {
 		cin >> v[i];
 	}
 
-	int idx = -1;	// negative -> positive
-	int ne = 0; int po = 0;	// negative, positive
-	for (int i = 0; i < n; i++) {
-		if (v[i] < 0) ne++;
-		else po++;
-
-		if (i != 0 && (v[i - 1] * v[i] < 0)) idx = i;
-	}
-
+	int l = 0; int r = n - 1;
 	long long res = INF;
-	long long x, y;
-	if (idx == -1) {
-		if (ne) cout << v[n - 2] << ' ' << v[n - 1];
-		else cout << v[0] << ' ' << v[1];
-		return 0;
-	}
+	long long x = v[l]; long long y = v[r];
 
-	res = abs(v[idx - 1] + v[idx]);
-	x = v[idx - 1];
-	y = v[idx];
-
-	if (ne >= 2 && res > abs(v[idx - 1] + v[idx - 2])) {
-		 res = abs(v[idx - 1] + v[idx - 2]);
-		 x = v[idx - 2];
-		 y = v[idx - 1];
-	}
-
-	if (po >= 2 && res > v[idx] + v[idx + 1]) {
-		res = v[idx] + v[idx + 1];
-		x = v[idx];
-		y = v[idx + 1];
-	}
-
-	int l = 0; 
-	int r = n - 1;
-	while (v[l] < 0 && v[r] > 0) {
-		if (res > abs(v[l] + v[r])) {
-			res = abs(v[l] + v[r]);
+	while (l < r) {
+		long long sum = v[l] + v[r];
+		if (res > abs(sum)) {
+			res = abs(sum);
 			x = v[l];
 			y = v[r];
 		}
-		if (-v[l] > v[r]) l++;
+
+		if (sum < 0) l++;
 		else r--;
 	}
 
