e06f9d762dc28ff28aafed62d4030c310fc95b30
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-07-04 23:19:12 +0900

    [Platinum IV] Title: 광고, Time: 8 ms, Memory: 7076 KB -BaekjoonHub

--- C++17/백준/Platinum/1305. 광고/광고.cc
+++ C++17/백준/Platinum/1305. 광고/광고.cc
@@ -13,18 +13,6 @@ auto get_fail = [](const string &s) {
 	return f;
 };
 
-auto kmp = [](const string &s, const string &t) {
-	vector ret(0, 0), f = get_fail(t);
-	for (int i = 0, j = 0; i < s.size(); i++) {
-		while(j && s[i] != t[j]) j = f[j - 1];
-		if (s[i] == t[j] && ++j == t.size()) {
-			ret.push_back(i - t.size() + 1);
-			j = f[j - 1];
-		}
-	}
-	return ret;
-};
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
@@ -32,8 +20,6 @@ int main() {
 
 	int l; cin >> l;
 	string s; cin >> s;
-	string t = s + s;
-	auto buc = kmp(t, s);
-	auto f = get_fail(t);
-	cout << buc[1] - f[buc[1] - 1];
+	auto f = get_fail(s);
+	cout << l - f[l - 1];
 }
